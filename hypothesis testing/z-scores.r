bc4543385d3176be5345b423746decd46697d99e
Tran P Bui <67050265+tranpbui@users.noreply.github.com>
2021-06-17 00:08:53 -0700

    Update z-scores.r

--- hypothesis testing/z-scores.r
+++ hypothesis testing/z-scores.r
@@ -1,3 +1,5 @@
+# This script is to show how I computed the z-scores for the hypothesis tests in the drug crimes.pdf file
+
 df_2017 <- df %>% 
   filter(year == 2017) 
   mean(df_2017$parent_group == "DRUG/NARCOTIC OFFENSES")
@@ -18,8 +20,8 @@ df_2018 <- df %>%
 
 # 2214
 # 75325
-# 24.75236
-# 21.82176
+
+
 
   
 Pe_2017 <- (2357+1000)/(73100+75790)
@@ -28,6 +30,7 @@ num_2017 <- 0.0322435 - 0.01319435
 denom_2017 <- sqrt(((Pe_2017*(1-Pe_2017))/73100) + ((Pe_2017*(1-Pe_2017))/75790))
              
 num_2017/denom_2017
+# 24.75236
 
 Pe_2018 <- (2214+1000)/(75325+75790)
 
@@ -35,5 +38,4 @@ num_2018 <- 0.02939263-0.01319435
 denom_2018 <- sqrt(((Pe_2018*(1-Pe_2018))/75325) + ((Pe_2018*(1-Pe_2018))/75790))
              
 num_2018/denom_2018
-
-
+# 21.82176
